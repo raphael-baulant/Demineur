c300f9b2301c1b8827ad54c3e4bc1f0de7794f19
matheoenee <messagerie.matheo@gmail.com>
2023-11-29 12:30:43 +0100

    scoreboard and record

--- scoreboard_function.c
+++ scoreboard_function.c
@@ -1,9 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "structures.h"
+#include "constants.h"
+#include "functions.h"
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
 // file to avoid conflict in functions.c and .h (add theses functions later in functions.c and .h)
 
 // function to add new record in scoreboard file (after win)
+void addRecord(char* username, int seconds){
+    const char *bannerFile = "scoreboard.txt";
+
+    // Ouvrir le fichier en mode lecture
+    FILE *fichier = fopen(bannerFile, "r");
+}
 
 // function to get the 3 best score from scoreboard file
 
 // function to print the podium (3 best score)
+void printScoreboard(){
+    const char *bannerFile = "banners/scoreboard.txt";
+
+    // Ouvrir le fichier en mode lecture
+    FILE *fichier = fopen(bannerFile, "r");
+
+    // Vérifier si l'ouverture du fichier a réussi
+    if (fichier == NULL) {
+        fprintf(stderr, "Impossible d'ouvrir le fichier %s\n", bannerFile);
+        exit;  // Quitter le programme avec un code d'erreur
+    }
+
+    // Lire et afficher le contenu du fichier ligne par ligne
+    char ligne[100];  // Choisir une taille appropriée pour la ligne
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        printf("%s", ligne);
+    }
+    printf("\n\n                          [+] Press \"Enter\" to play.\n");
+
+    // Fermer le fichier
+    fclose(fichier);
+}
 
 // 
