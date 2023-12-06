b4fd5c334cdb41ffbd011a31338a4078f85aaec0
matheoenee <messagerie.matheo@gmail.com>
2023-12-06 12:08:40 +0100

    leaderboard function (appart  file)

--- scoreboard_function.c
+++ scoreboard_function.c
@@ -7,8 +7,14 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
-// file to avoid conflict in functions.c and .h (add theses functions later in functions.c and .h)
-// ATTENTION A LA DIFFICULTEE !!!!
+
+#define MAX_SCORES 100
+
+// Structure pour stocker les scores et les noms d'utilisateur
+struct Record {
+    char username[50];
+    int time;
+};
 
 // function to get the 3 best score from scoreboard file
 
@@ -37,4 +43,81 @@ void printScoreboard(){
     fclose(fichier);
 }
 
-// 
+int getNumberOfRecordsByDifficulty(int targetDifficulty){
+    int lines = 0;
+    int difficulty;
+
+    FILE *fichier = fopen("record.txt", "r");
+    if (fichier == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return NULL;
+    }
+
+    while (fscanf(fichier, "%d:%*49[^:]:%*d", &difficulty) == 1) {
+        // Afficher les valeurs sous la forme "[difficulty] username - timer"
+        if(difficulty == targetDifficulty)
+            lines++;
+    }
+
+    return lines;
+}
+
+int compareRecords(const void *a, const void *b) {
+    return ((struct Record *)a)->time - ((struct Record *)b)->time;
+}
+
+struct Record* getSortedRecords(int targetDifficulty) {
+    
+    int lines = getNumberOfRecordsByDifficulty(targetDifficulty);
+    int difficulty;
+    char username[50];
+    int time;
+
+    FILE *fichier = fopen("record.txt", "r");
+    if (fichier == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return NULL;
+    }
+
+    struct Record* RecordsList = malloc(sizeof(struct Record) * lines);
+
+    // Réinitialiser le curseur du fichier
+    fseek(fichier, 0, SEEK_SET);
+    int i = 0;
+
+    // Lire le fichier ligne par ligne
+    while (fscanf(fichier, "%d:%49[^:]:%d", &difficulty, username, &time) == 3) {
+        // Afficher les valeurs sous la forme "[difficulty] username - timer"
+        if(difficulty == targetDifficulty){
+            strcpy(RecordsList[i].username, username);
+            RecordsList[i].time = time;
+            i++;
+        }
+    }
+    fclose(fichier);
+    qsort(RecordsList, lines, sizeof(struct Record), compareRecords);
+    return RecordsList;
+}
+
+void printBestRecord(int targetDifficulty){
+    struct Record* sortedRecords = getSortedRecords(targetDifficulty);
+    int numberOfRecords = getNumberOfRecordsByDifficulty(targetDifficulty);
+
+    if(numberOfRecords>3){
+        for(int i=0; i<3; i++){
+            printf("    #%d %s %d\n",i,sortedRecords[i].username, sortedRecords[i].time);
+        }
+    }
+    else{
+        for(int i=0; i<numberOfRecords; i++){
+            printf("    #%d %s %d\n",i,sortedRecords[i].username, sortedRecords[i].time);
+        }
+    }
+}
+
+int main() {
+    int targetDifficulty = 18;
+    printBestRecord(targetDifficulty);
+
+    return 0;
+}
